7da203ebfcdc163981712f3a8dff8a4b6d8c8377
Hasnainqari <100525567+Hasnainqari@users.noreply.github.com>
2022-05-25 17:56:05 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,230 @@
+// Chapter No 9-11 start;
+//  Question NO 01;
+// var city =prompt("Enter Your city name");
+// if(city === "Karachi"){
+//     alert("Welcome to city of lights")
+// }
+
+// Question No 02;
+// var gender =prompt("Enter your gender");
+// if(gender === "male"){
+//     alert("Good Morning Sir!")
+// }
+// else if(gender === "female"){
+//     alert("Good Morning Ma’am")
+// }
+// else if(gender === ""){
+//     alert("Please enter your gender!")
+// }
+
+// Question No 03;
+// var signal = prompt("Enter signal color")
+// if(signal === "red"){
+//     alert("Must stop!")
+// }
+// else if (signal === "yellow"){
+//     alert("Ready to move.")
+// }
+// else if (signal === "green"){
+//     alert("Move now.")
+// }
+// else if (signal === ""){
+//     alert("Please enter color name!")
+// }
+
+// Question No 04;
+// var fuel = prompt("Enter Fuel No. Do you want to?");
+// if (fuel <= 0.25){
+//     alert("Please refill the fuel in your car")
+// }
+
+// Question No 05;
+// var secret =prompt("Enter a number");
+// if(secret === "2"){
+//     alert(" Bingo! Correct answer")
+// }
+// else if (secret === "3"){
+//     alert("Close enough to the correct answer.")
+// }
+
+// Question No 06;
+// var divide =prompt("Enter a number that number divisible by ");
+// if (divide === "3"){
+//     alert("The number is divisible by 3.")
+// }
+// else if (divide === "1"){
+//     alert("The Number is not divisible by 3")
+// }
+
+// Question No 07;
+// var number = prompt("Enter any number. ");
+// if (number === "2"){
+//     alert("This number is even number")
+// }
+// else if (number === "1"){
+//     alert("This number is odd number")
+// }
+
+// Question No 08;
+// var tem =prompt("Enter a temprature number.")
+// if(tem === "40"){
+//     alert("It is too hot outside.")
+// }
+// else if (tem === "30"){
+//     alert("The Weather today is Normal.")
+// }
+// else if (tem === "20"){
+//     alert(" Today's the Weather is cool.")
+// }
+// else if (tem === "10"){
+//     alert("OMG! Today’s weather is so Cool.")
+// }
+
+// Question No 09;
+// var calculateNum1 = +prompt("Enter num 1");
+// var calculateNum2 = +prompt("Enter num 2");
+// var opt = prompt("Enter your opt (+ - / *)");
+// var result;
+
+// if(opt === "+"){
+//     result = calculateNum1 + calculateNum2
+// }
+
+// if(opt === "-"){
+//     result = calculateNum1 - calculateNum2
+// }
+
+// if(opt === "*"){
+//     result = calculateNum1 * calculateNum2
+// }
+
+// if(opt === "/"){
+//     result = calculateNum1 / calculateNum2
+// }
+// if(opt === "%"){
+//     result = calculateNum1 / calculateNum2
+// }
+// alert(result);
+
+// chapter No 9-11 complete;
+
+
+// chapter No 12-13 Start;
+// Question No 01;
+// var upperCase =prompt("Enter a letter");
+// if (upperCase === "A"){
+//     alert(65)
+// }
+// else if (upperCase === "Z"){
+//     alert(90)
+// }
+// else if (upperCase === "a"){
+//     alert(97)
+// }
+// else if (upperCase === "z"){
+//     alert(122)
+// }
+
+// Question No 02;
+// var integer =prompt("Enter a integers");
+// if (integer === "23"){
+//     alert ("The two integer are equal")
+// }
+// else if (integer === "2"){
+//     alert ("Accept atleast two integer")
+// }
+
+// Question No 03;
+// var positive =prompt("Enter a number");
+// if (positive === "1"){
+//     alert ("Positive Number")
+// }
+// else if (positive === "-1"){
+//     alert ("Negative Number")
+// }
+// else if (positive === "0"){
+//     alert ("zero")
+// }
+
+// Question No 04;
+// var vowel =prompt("Enter a vowel")
+// if(vowel === "a"){
+//     alert("True")
+// }
+// else if (vowel === "b"){
+//     alert("false")
+// }
+
+// Question No 05;
+// var password =prompt("Enter You Password");
+// if(password === "username123"){
+//     alert("Correct! The password you entered matches the original password.")
+// }
+// else if (password === "useername 2e21"){
+//     alert("Incorrect password!!!")
+// }
+// else if(password === ""){
+//     alert("Please enter your password")
+// }
+
+// Question No 06;
+// var hour =prompt("Enter number");
+// if(hour<18){
+//     alert("Good day")
+// }
+// else 
+// alert("Good evening")
+
+// chapter No 12-13 complete;
+
+// chapter No 14-16 Start;
+
+// Question No 01;
+// var cities = [];
+
+// cities[0] = "Karachi";
+// console.log(cities);
+
+
+// Question No 02;
+// var cities = new Array;
+
+// cities[0] = "Karachi";
+// console.log(cities);
+
+// Question No 03;
+// var str = [];
+// str [0] = "Hasnain";
+// console.log(str);
+
+// // Question No 04;
+// var nmb = [];
+// nmb [0] = 100;
+// console.log(nmb);
+
+// Question No 05;
+// var bol = [];
+// bol [0] = true;
+// console.log(bol);
+
+// Question No 06;
+// var mix = [];
+// mix [0] = "Hasnain";
+// mix [1] = 123;
+// mix [2] = true;
+// mix [3] = false;
+// console.log(mix);
+
+// Question No 07;
+// var qual = [];
+// qual [0] = "SSC";
+// qual [1] = "HSC";
+// qual [2] = "BCS";
+// qual [3] = "BS";
+// qual [4] = "BCOM";
+// qual [5] = "MS";
+// qual [6] = "M.Phil."
+// qual [7] = "PHD";
+// console.log(qual);
+
+// Question No 08;
